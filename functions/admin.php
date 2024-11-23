32986c54913aaed36d56c8bf42c8d6c84364a277
Kia <kiasambrook@btinternet.com>
2024-11-23 21:45:32 +0000

    feat: improvements on ninja form widget

--- functions/admin.php
+++ functions/admin.php
@@ -33,9 +33,12 @@ add_action('wp_dashboard_setup', 'ninja_form_dashboard_widget');
 function ninja_forms_dashboard_widget_content() {
     echo '<p>Here are the latest submissions from the contact form:</p>';
     $subs = Ninja_Forms()->form(1)->get_subs();
+    // limit to 5 submissions
+    $subs = array_slice($subs, 0, 5);
     echo '<ul>';
     foreach ($subs as $sub) {
-        echo '<li>' . $sub->get_field_value('name') . ' - ' . $sub->get_field_value('email') . '</li>';
+        // link to submission message
+        echo '<li><a href="' . admin_url('admin.php?page=ninja-forms-subs&form_id=1&sub_id=' . $sub->get_id()) . '">' . $sub->get_field_value('name') . ' | ' . $sub->get_sub_date() . '</a></li>';
     }
     echo '</ul>';
 }
